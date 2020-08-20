1537cfaf2c37306e5d9ef908e317682af1574b3e
zzg <997009743@qq.com>
2020-08-20 22:35:18 +0800

    第一次提交版本

---
+++ git-test1/src/com/ithei/test/GitClass.java
@@ -0,0 +1,15 @@
+package com.ithei.test;
+
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
